fe20a845a71c876308438933d91bd6dfdda1f15c
skl2208 <lsukuem@gmail.com>
2025-02-18 23:12:16 +0700

    Commit from Home

---
+++ ws/addmqtt.php
@@ -0,0 +1,56 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+
+include "config.php";
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+if (json_last_error() === JSON_ERROR_NONE) {
+    $pid = $data["pid"];
+    $msg = $data["msg"];
+    if (isset($data["unread"])) {
+        $unread = $data["unread"];
+    } else {
+        $unread = "";
+    }
+    if (isset($data["id"])) {
+        $id = $data["id"];
+    } else {
+        $id = "";
+    }
+
+    try {
+        if ($id == "" || $id == null) {
+            //=== Insert ===//
+            $sql = "INSERT INTO mqtt (pid,msg) VALUES (?,?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('ss', $pid, $msg);
+            $message = "Insert Successfully";
+        } else {
+            //=== Update ===//
+            $sql = "UPDATE mqtt SET pid = ?, msg = ?,unread=?,updatetime=CURRENT_TIMESTAMP() WHERE id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param('sssi', $pid, $msg, $unread, $id);
+            $message = "Update Successfully, id = $id, pid = $pid, unread = $unread";
+        }
+
+        if ($stmt->execute()) {
+            $list_result["status"] = "OK";
+            $list_result["message"] = $message;
+        } else {
+            $list_result["status"] = "ERROR";
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+        $list_result["status"] = "ERROR";
+        $list_result["message"] = $e->getMessage();
+    }
+}
+
+echo json_encode($list_result);
+$conn->close();
---
+++ ws/config.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+$servername = "localhost";
+$username = "cp187059_admin";
+$password = "7QhZD, ]i[^!o";
+$dbname = "cp187059_sk";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+mysqli_set_charset($conn, "utf8");
+
+// Check connection
+
+if ($conn->connect_error) {
+
+  die("ไม่สามารถเชื่อมต่อฐานข้อมูล : " . $conn->connect_error);
+
+} 
+$baseHTTP = "http://";
+
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+
+  $baseHTTP = "https://";
+
+}
+$baseURL = $_SERVER['SERVER_NAME']."/";
+
+
+?>
